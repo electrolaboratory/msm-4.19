58f4daa9247b5ef4051ecd08ed5d54c972a55c98
texascake soft <budalblimbing@gmail.com>
2025-03-10 14:58:04 +0000

    fs: Drop KSU second way

--- fs/namespace.c
+++ fs/namespace.c
@@ -32,10 +32,6 @@
 #include "pnode.h"
 #include "internal.h"
 
-#ifdef CONFIG_KSU
-#include <linux/ksu.h>
-#endif
-
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
 extern bool susfs_is_current_ksu_domain(void);
 extern bool susfs_is_current_zygote_domain(void);
